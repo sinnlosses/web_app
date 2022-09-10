f5ee99ed93423a86714a6fb143f05d7a64c17bbd
fuji <fujiyoshiyuki@gmail.com>
2022-09-11 05:11:43 +0900

    remote containerでのフロントエンド、バックエンドの独立起動環境の構築

---
+++ backend/mysite/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
