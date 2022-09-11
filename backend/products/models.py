14fe63ccd17d538ebe12986ce7dcbdd0e9ad5399
fuji <fujiyoshiyuki@gmail.com>
2022-09-12 01:24:02 +0900

    APIテスト用フォルダ追加

---
+++ backend/products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
